e975512f8603f5729fb554fe6e920fa5034228d5
xugaung <xuguang@pcitech.com>
2024-01-12 14:46:17 +0800

    fix: 添加自动导入插件

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport(),
   ],
   resolve: {
     alias: {
